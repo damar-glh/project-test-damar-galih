c732eeda43ded8fea9c5f6ce7f019a5a31bafa9d
Damar Galih <damarmuhamad13@gmail.com>
2025-07-19 02:59:30 +0700

    fix: add deployment github pages redirect

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+onMounted (() => {
+  const redirectPath = new URLSearchParams(window.location.search).get('redirect')
+  if (redirectPath) {
+    router.replace(redirectPath)
+  }
+})
+</script>
 
 <template>
   <router-view />
